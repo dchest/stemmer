0e4cc015434f34b74dbfd9c1e30e2e1770d621c2
Dmitry Chestnykh <dmitry@codingrobots.com>
2012-10-01 11:52:45 +0200

    Change license from MIT to BSD-like.
    Call authors "The Stemmer Package Authors" and move copyright holders
    names to AUTHORS file.

--- porter2/porter2_test.go
+++ porter2/porter2_test.go
@@ -1,5 +1,5 @@
-// Copyright (c) 2011 Dmitry Chestnykh
-// Use of this source code is governed by a MIT-style
+// Copyright 2012 The Stemmer Package Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 package porter2
